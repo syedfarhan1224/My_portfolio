1f307c8c027e3de95de141e2d8eaa4bddae19648
syedfarhan1224 <syedfarhan1224398@gmail.com>
2025-02-04 02:15:50 +0500

    make a build

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Experiance from './components/Experiance';
-import Projects from './components/project';
+import Projects from "./components/Projects"
 import Footer from './components/Footer';
 import ContactMe from './components/contact';
 
